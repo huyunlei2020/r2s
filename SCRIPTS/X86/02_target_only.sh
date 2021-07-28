9cd3b198aa7ed3a86a96c1dac21b4fa221713a13
a0575 <68583179+a0575@users.noreply.github.com>
2021-07-28 22:21:37 +0800

    Add files via upload

--- SCRIPTS/X86/02_target_only.sh
+++ SCRIPTS/X86/02_target_only.sh
@@ -2,11 +2,26 @@
 
 # 使用 O2 级别的优化
 sed -i 's/O3/O2/g' include/target.mk
+sed -i 's/-Wl,--gc-sections//' include/target.mk
 
 # 在 X86 架构下移除 Shadowsocks-rust
 sed -i '/Rust:/d' package/lean/luci-app-ssr-plus/Makefile
 sed -i '/Rust:/d' package/new/luci-app-passwall/Makefile
 sed -i '/Rust:/d' package/lean/luci-app-vssr/Makefile
+
+# 内核加解密组件
+echo '
+CONFIG_CRYPTO_AES_NI_INTEL=y
+' >> ./target/linux/x86/64/config-5.4
+
+# MPTCP
+#echo '
+#CONFIG_MPTCP=y
+#CONFIG_MPTCP_PM_ADVANCED=y
+#CONFIG_MPTCP_FULLMESH=y
+#CONFIG_DEFAULT_FULLMESH=y
+#CONFIG_DEFAULT_MPTCP_PM="fullmesh"
+#' >> ./target/linux/x86/64/config-5.4
 
 #Vermagic
 latest_version="$(curl -s https://github.com/openwrt/openwrt/releases |grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" |sed -n '/21/p' |sed -n 1p |sed 's/v//g' |sed 's/.tar.gz//g')"
